7a8e65fc015b0ca33e741d8adfab3a1ccb85c953
Antoine Joly <jolyantoine@pm.me>
2021-01-25 17:54:35 +0100

    Update Category.vue

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -205,7 +205,7 @@ export default {
 }
 .subcategory-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
   grid-template-rows: repeat(auto-fit, minmax(250px, 1fr));
   gap: 10px;
 
