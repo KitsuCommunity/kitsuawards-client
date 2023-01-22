7c725da487096731fb9cec560e99759ed151d481
Reinachan <github@reinina.moe>
2023-01-23 00:14:20 +0100

    Make videos playable

--- src/components/Media/Gif.tsx
+++ src/components/Media/Gif.tsx
@@ -1,3 +1,5 @@
+import styles from './media.module.css';
+
 interface GifProps {
   media: string;
 }
@@ -6,7 +8,7 @@ const Gif = ({ media }: GifProps) => {
   const webm = media.replace(/\.gifv/, '.webm');
   const mp4 = media.replace(/\.gifv/, '.mp4');
   return (
-    <video controls loop>
+    <video controls loop className={styles.video}>
       <source src={webm} type="video/webm" />
       <source src={mp4} type="video/mp4" />
     </video>
