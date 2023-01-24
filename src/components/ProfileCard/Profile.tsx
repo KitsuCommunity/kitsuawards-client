6eaaca737deb131e9657fa662a05b7edee8f2d0f
Nina Louise <github@reinina.moe>
2023-01-24 07:10:10 +0100

    Add default profile picture in case of null

--- src/components/ProfileCard/Profile.tsx
+++ src/components/ProfileCard/Profile.tsx
@@ -17,7 +17,7 @@ export const ProfileCard = ({ className, closeNav }: ProfileCardProps) => {
     <>
       {!!user.user ? (
         <div className={[styles.account, className].join(' ')}>
-          <img src={user.user.avatarImage.views[0].url} />
+          <img src={user.user.avatarImage?.views[0]?.url ?? 'https://kitsu.io/images/default_avatar-2ec3a4e2fc39a0de55bf42bf4822272a.png'} />
           <span>
             {user.user.name}{' '}
             {user.role !== Role.Regular ? `-- ${user.role}` : ''}
