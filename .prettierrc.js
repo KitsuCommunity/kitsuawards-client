8f5560d3d4678c6457aaf2173eb34032abd78a00
Reinachan <github@reinina.moe>
2023-11-27 23:44:01 +0100

    switch prettierrc to yml config

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  semi: true,
-  singleQuote: true,
-  printWidth: 80, // Useful when viewing multiple files side-by-side
-  tabWidth: 2,
-  arrowParens: 'always', // useful for TypeScript and recommended by Prettier
-  trailingComma: 'all', // consistency, consistency, consistency
-};
