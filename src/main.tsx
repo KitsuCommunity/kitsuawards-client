33850c23d3b9a6d1ba3b5b0ea710b2d1eda48010
Reinachan <github@reinina.moe>
2023-11-27 23:59:37 +0100

    update renderer to preact renderer

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'preact/debug';
+import { render } from 'preact';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'urql';
 import { client } from './apiConfig';
 import 'styles/index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider value={client}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+render(
+  <Provider value={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')!,
 );
