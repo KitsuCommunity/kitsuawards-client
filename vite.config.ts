74e3a11cfd0e8555feeedb4114fe2ba55ffbe5a6
Reinachan <github@reinina.moe>
2023-11-29 18:42:42 +0100

    fix stylelint and add import sorting to eslint also updates various packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
 import path from 'path';
+
+import { preact } from '@preact/preset-vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
